038a15ae8c1f309cb7d34de5170af8b5b7f2f6a4
Ella Jackson <jacksone2934@student.faytechcc.edu>
2026-09-13 06:08:28 +0000

    M2T2 Halfway done

---
+++ m2t2_jackson.cpp
@@ -0,0 +1,39 @@
+// CSC 134
+// M2T2 - Receipt Calculator
+// Ella Jackson
+// 9/13/26
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main () {
+    // Purpose - Create a simple receipt
+    // should also handle sales tax (8%)
+
+    // Declare variables
+    string item = "Baked ziti pasta dish";
+    double item_price = 5.99;
+    double tax_percent = 0.08;  // 8% is 8/100
+    double tax_amount;          // tax in $
+    double total;         // price + tax
+
+
+    // Greet the user and take the order
+    cout << "Welcome to our CSC 134 restaurant!" << endl;
+    cout << "You ordered one " << item << "." << endl;
+
+    // Calculate the meal price
+    // Calculate the sales tax and the total price
+    tax_amount = item_price * tax_percent; // take 8% of the item price
+    total = item_price + tax_amount;
+
+
+
+    
+    // Print the receipt
+    cout << fixed << setprecision(2);
+    cout << total << endl;
+
+    return 0; // no errors
+}
